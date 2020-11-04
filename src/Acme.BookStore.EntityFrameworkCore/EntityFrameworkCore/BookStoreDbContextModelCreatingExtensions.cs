4ac0048545c2d996a1a3a62bf1d01e53d3f8648c
min8grace <56316476+min8grace@users.noreply.github.com>
2020-11-03 16:41:48 -0800

    Add project files.

---
+++ src/Acme.BookStore.EntityFrameworkCore/EntityFrameworkCore/BookStoreDbContextModelCreatingExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Acme.BookStore.Authors;
+using Acme.BookStore.Books;
+using Microsoft.EntityFrameworkCore;
+using Volo.Abp;
+using Volo.Abp.EntityFrameworkCore.Modeling;
+
+namespace Acme.BookStore.EntityFrameworkCore
+{
+    public static class BookStoreDbContextModelCreatingExtensions
+    {
+        public static void ConfigureBookStore(this ModelBuilder builder)
+        {
+            Check.NotNull(builder, nameof(builder));
+
+            /* Configure your own tables/entities inside here */
+
+            builder.Entity<Book>(x =>
+            {
+                x.ToTable(BookStoreConsts.DbTablePrefix + "Books", BookStoreConsts.DbSchema);
+                x.ConfigureByConvention();//auto configure for the base class props
+                x.Property(x => x.Name).IsRequired().HasMaxLength(128);
+                // ADD THE MAPPING FOR THE RELATION
+                x.HasOne<Author>().WithMany().HasForeignKey(x => x.AuthorId).IsRequired();
+            });
+
+            builder.Entity<Author>(b =>
+            {
+                b.ToTable(BookStoreConsts.DbTablePrefix + "Authors",
+                    BookStoreConsts.DbSchema);
+
+                b.ConfigureByConvention();
+
+                b.Property(x => x.Name)
+                    .IsRequired()
+                    .HasMaxLength(AuthorConsts.MaxNameLength);
+
+                b.HasIndex(x => x.Name);
+            });
+            //builder.Entity<YourEntity>(b =>
+            //{
+            //    b.ToTable(BookStoreConsts.DbTablePrefix + "YourEntities", BookStoreConsts.DbSchema);
+            //    b.ConfigureByConvention(); //auto configure for the base class props
+            //    //...
+            //});
+        }
+    }
+}
